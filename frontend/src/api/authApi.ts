b47e4961e02c2afb30c43541d8e42493d80c0d83
erllocaylocay <117888162+erllocaylocay@users.noreply.github.com>
2025-03-12 11:34:37 +0800

    register-login-api-checked

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5156/api/auth"; // Ensure this matches your backend port
+  import.meta.env.VITE_API_URL || "http://localhost:5156/api/auth";
 
 export const registerUser = async (
   username: string,
